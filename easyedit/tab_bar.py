f93e767e106c0b8c36fbb9bda762589dc8c65eec
Matthew S. Klosak <msklosak@gmail.com>
2017-07-19 00:03:22 -0500

    Remove unnecessary statements and unused import.

--- easyedit/tab_bar.py
+++ easyedit/tab_bar.py
@@ -1,4 +1,3 @@
-from PyQt5.QtCore import pyqtSignal
 from PyQt5.QtWidgets import QApplication, QTabWidget
 
 from easyedit.text_area import TextArea
--- easyedit/text_area.py
+++ easyedit/text_area.py
@@ -4,19 +4,15 @@ from PyQt5.QtWidgets import QTextEdit
 
 
 class TextArea(QTextEdit):
-    unsaved_changes = pyqtSignal(str)
     cursor_position_changed = pyqtSignal()
 
     def __init__(self):
         super().__init__()
 
-        self.unsaved_changes = False
-
         self.setAcceptRichText(False)
 
         self.update_font(self.font())
 
-        self.textChanged.connect(self.set_unsaved_changes)
         self.cursorPositionChanged.connect(self.cursor_position_changed)
 
     def update_font(self, new_font):
@@ -25,6 +21,3 @@ class TextArea(QTextEdit):
         font_metrics = QFontMetrics(new_font)
 
         self.setTabStopWidth(font_metrics.width("    "))
-
-    def set_unsaved_changes(self):
-        self.unsaved_changes = True
